fc586383839d07306308675de9a6c129b8e5c5f7
GaozyDev <670453367@qq.com>
2021-03-14 04:15:53 +0800

    修改cron表达式

--- src/main/java/com/gzy/quickapi/ps5/ScheduleTask.java
+++ src/main/java/com/gzy/quickapi/ps5/ScheduleTask.java
@@ -1,10 +1,14 @@
 package com.gzy.quickapi.ps5;
 
 import com.gzy.quickapi.ps5.enums.PS5TypeEnum;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
+
+import java.util.Date;
 
 @Configuration
 @EnableScheduling
@@ -13,8 +17,11 @@ public class ScheduleTask {
     @Autowired
     PS5Service ps5Service;
 
-    @Scheduled(cron = "0 0 0-23 * * ?")
+    private static final Logger logger = LoggerFactory.getLogger(ScheduleTask.class.getName());
+
+    @Scheduled(cron = "0 0 0/1 * * ?")
     private void configureTasks() {
+        logger.info("定时爬虫：" + new Date());
         PS5Service.opticalDrivePriceData = ps5Service.getPS5ProductData(PS5TypeEnum.OPTICAL_DRIVE);
         PS5Service.digitalEditionPriceData = ps5Service.getPS5ProductData(PS5TypeEnum.DIGITAL_EDITION);
     }
