478125f5ebd512fbdb21f8c6ea1d653e76a3d489
GaozyDev <670453367@qq.com>
2021-03-17 11:56:54 +0800

    WebCrawler

---
+++ src/main/java/com/gzy/quickapi/ps5/crawler/WebCrawler.java
@@ -0,0 +1,107 @@
+package com.gzy.quickapi.ps5.crawler;
+
+import com.gzy.quickapi.Constant;
+import com.gzy.quickapi.ps5.dto.ProductPriceInfo;
+import com.gzy.quickapi.ps5.service.PriceService;
+import com.ruiyun.jvppeteer.core.Puppeteer;
+import com.ruiyun.jvppeteer.core.browser.Browser;
+import com.ruiyun.jvppeteer.core.browser.BrowserFetcher;
+import com.ruiyun.jvppeteer.core.page.ElementHandle;
+import com.ruiyun.jvppeteer.core.page.Page;
+import com.ruiyun.jvppeteer.options.LaunchOptions;
+import com.ruiyun.jvppeteer.options.LaunchOptionsBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+@Component
+public class WebCrawler {
+
+    private static final Logger logger = LoggerFactory.getLogger(WebCrawler.class.getName());
+
+    @Autowired
+    private Constant constant;
+
+    public List<ProductPriceInfo> startWebCrawler(String url) {
+        List<ProductPriceInfo> productPriceInfoList = new ArrayList<>();
+        try {
+            Browser browser = launchBrowser();
+            Page page = browser.newPage();
+            page.goTo(url);
+            parsePage(page, productPriceInfoList);
+
+            boolean hasNextPage;
+            List<ElementHandle> pages = page.$$("#J_feed_pagenation li");
+            ElementHandle elementHandle = pages.get(pages.size() - 1);
+            String text = (String) elementHandle.$eval("a", "node => node.innerText", new ArrayList<>());
+            hasNextPage = "下一页".equals(text);
+            while (hasNextPage) {
+                elementHandle.click();
+                parsePage(page, productPriceInfoList);
+                pages = page.$$("#J_feed_pagenation li");
+                elementHandle = pages.get(pages.size() - 1);
+                text = (String) elementHandle.$eval("a", "node => node.innerText", new ArrayList<>());
+                hasNextPage = "下一页".equals(text);
+            }
+
+            page.close();
+            browser.close();
+
+            productPriceInfoList.sort(Comparator.comparing(ProductPriceInfo::getPrice));
+        } catch (InterruptedException | IOException | ExecutionException e) {
+            e.printStackTrace();
+            logger.error("[浏览器错误]" + e);
+        }
+
+        return productPriceInfoList;
+    }
+
+    private Browser launchBrowser() throws InterruptedException, ExecutionException, IOException {
+        BrowserFetcher.downloadIfNotExist(null);
+        ArrayList<String> argList = new ArrayList<>();
+        argList.add("--no-sandbox");
+        argList.add("--disable-setuid-sandbox");
+        LaunchOptions options = new LaunchOptionsBuilder().withArgs(argList).withHeadless(true)
+                .withExecutablePath(constant.getChromePath()).build();
+        return Puppeteer.launch(options);
+    }
+
+    private void parsePage(Page page, List<ProductPriceInfo> productPriceInfoList) {
+        try {
+            page.waitForSelector("#feed-main-list");
+            List<ElementHandle> elementHandles = page.$$("#feed-main-list .feed-row-wide");
+            for (ElementHandle elementHandle : elementHandles) {
+                try {
+                    ElementHandle tag = elementHandle.$(".feed-block span.search-pastdue-mark");
+                    if (tag != null) {
+                        // 商品过期，售罄
+                        continue;
+                    }
+
+                    String title = (String) elementHandle.$eval(".feed-block h5.feed-block-title a.feed-nowrap", "node => node.innerText", new ArrayList<>());
+                    String priceText = (String) elementHandle.$eval(".feed-block h5.feed-block-title div.z-highlight", "node => node.innerText", new ArrayList<>());
+                    String platform = (String) elementHandle.$eval(".feed-block div.z-feed-foot span.feed-block-extras span", "node => node.innerText", new ArrayList<>());
+                    String[] text = priceText.split("元");
+                    if (text.length >= 1) {
+                        double price = Double.parseDouble(text[0]);
+                        ProductPriceInfo productPriceInfo = new ProductPriceInfo(title, price, platform);
+                        productPriceInfoList.add(productPriceInfo);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    logger.error("[HTML解析错误]" + e);
+                }
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            logger.error("[抓包错误]" + e);
+        }
+    }
+}
--- src/main/java/com/gzy/quickapi/ps5/service/PriceService.java
+++ src/main/java/com/gzy/quickapi/ps5/service/PriceService.java
@@ -1,39 +1,27 @@
 package com.gzy.quickapi.ps5.service;
 
-import com.gzy.quickapi.Constant;
+import com.gzy.quickapi.ps5.crawler.WebCrawler;
+import com.gzy.quickapi.ps5.dataobject.ProductPrice;
 import com.gzy.quickapi.ps5.dto.ProductPriceInfo;
 import com.gzy.quickapi.ps5.dto.ProductPriceInfos;
-import com.gzy.quickapi.ps5.dataobject.ProductPrice;
 import com.gzy.quickapi.ps5.enums.PS5TypeEnum;
 import com.gzy.quickapi.ps5.repository.ProductPriceInfoRepository;
 import com.gzy.quickapi.ps5.utils.KeyUtil;
-import com.ruiyun.jvppeteer.core.Puppeteer;
-import com.ruiyun.jvppeteer.core.browser.Browser;
-import com.ruiyun.jvppeteer.core.browser.BrowserFetcher;
-import com.ruiyun.jvppeteer.core.page.ElementHandle;
-import com.ruiyun.jvppeteer.core.page.Page;
-import com.ruiyun.jvppeteer.options.LaunchOptions;
-import com.ruiyun.jvppeteer.options.LaunchOptionsBuilder;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
-import java.math.BigDecimal;
-import java.util.*;
-import java.util.concurrent.ExecutionException;
+import java.util.Date;
+import java.util.List;
+import java.util.OptionalDouble;
 
 @Service
 public class PriceService {
 
     @Autowired
-    private Constant constant;
+    private WebCrawler webCrawler;
 
     @Autowired
     private ProductPriceInfoRepository productPriceInfoRepository;
-
-    private static final Logger logger = LoggerFactory.getLogger(PriceService.class.getName());
 
     // 光驱版
     public static ProductPriceInfos opticalDriveProductPriceInfos;
@@ -49,7 +37,7 @@ public class PriceService {
             url = "https://search.smzdm.com/?c=home&s=ps5%E6%95%B0%E5%AD%97%E7%89%88&brand_id=249&min_price=3000&max_price=5000&v=b&p=1";
         }
 
-        List<ProductPriceInfo> productPriceInfoList = startWebCrawler(url);
+        List<ProductPriceInfo> productPriceInfoList = webCrawler.startWebCrawler(url);
 
         ProductPriceInfos productPriceInfos = new ProductPriceInfos();
         productPriceInfos.setUpdateDate(new Date());
@@ -70,82 +58,6 @@ public class PriceService {
         productPriceInfoRepository.save(productPrice);
 
         return productPriceInfos;
-    }
-
-    public List<ProductPriceInfo> startWebCrawler(String url) {
-        List<ProductPriceInfo> productPriceInfoList = new ArrayList<>();
-        try {
-            Browser browser = launchBrowser();
-            Page page = browser.newPage();
-            page.goTo(url);
-            parsePage(page, productPriceInfoList);
-
-            boolean hasNextPage;
-            List<ElementHandle> pages = page.$$("#J_feed_pagenation li");
-            ElementHandle elementHandle = pages.get(pages.size() - 1);
-            String text = (String) elementHandle.$eval("a", "node => node.innerText", new ArrayList<>());
-            hasNextPage = "下一页".equals(text);
-            while (hasNextPage) {
-                elementHandle.click();
-                parsePage(page, productPriceInfoList);
-                pages = page.$$("#J_feed_pagenation li");
-                elementHandle = pages.get(pages.size() - 1);
-                text = (String) elementHandle.$eval("a", "node => node.innerText", new ArrayList<>());
-                hasNextPage = "下一页".equals(text);
-            }
-
-            page.close();
-            browser.close();
-
-            productPriceInfoList.sort(Comparator.comparing(ProductPriceInfo::getPrice));
-        } catch (InterruptedException | IOException | ExecutionException e) {
-            e.printStackTrace();
-            logger.error("[浏览器错误]" + e);
-        }
-
-        return productPriceInfoList;
-    }
-
-    private Browser launchBrowser() throws InterruptedException, ExecutionException, IOException {
-        BrowserFetcher.downloadIfNotExist(null);
-        ArrayList<String> argList = new ArrayList<>();
-        argList.add("--no-sandbox");
-        argList.add("--disable-setuid-sandbox");
-        LaunchOptions options = new LaunchOptionsBuilder().withArgs(argList).withHeadless(true)
-                .withExecutablePath(constant.getChromePath()).build();
-        return Puppeteer.launch(options);
-    }
-
-    private void parsePage(Page page, List<ProductPriceInfo> productPriceInfoList) {
-        try {
-            page.waitForSelector("#feed-main-list");
-            List<ElementHandle> elementHandles = page.$$("#feed-main-list .feed-row-wide");
-            for (ElementHandle elementHandle : elementHandles) {
-                try {
-                    ElementHandle tag = elementHandle.$(".feed-block span.search-pastdue-mark");
-                    if (tag != null) {
-                        // 商品过期，售罄
-                        continue;
-                    }
-
-                    String title = (String) elementHandle.$eval(".feed-block h5.feed-block-title a.feed-nowrap", "node => node.innerText", new ArrayList<>());
-                    String priceText = (String) elementHandle.$eval(".feed-block h5.feed-block-title div.z-highlight", "node => node.innerText", new ArrayList<>());
-                    String platform = (String) elementHandle.$eval(".feed-block div.z-feed-foot span.feed-block-extras span", "node => node.innerText", new ArrayList<>());
-                    String[] text = priceText.split("元");
-                    if (text.length >= 1) {
-                        double price = Double.parseDouble(text[0]);
-                        ProductPriceInfo productPriceInfo = new ProductPriceInfo(title, price, platform);
-                        productPriceInfoList.add(productPriceInfo);
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    logger.error("[HTML解析错误]" + e);
-                }
-            }
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-            logger.error("[抓包错误]" + e);
-        }
     }
 
     private double calAveragePrice(List<ProductPriceInfo> productPriceInfoList) {
